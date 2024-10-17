6c03ad52bfb5553003d5d71600d290d6d4691e14
marklovefap <hok31656@gmail.com>
2024-10-17 12:36:26 +0700

    remove api part

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Fetch from "./Fetch.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Fetch />
+    <App />
   </StrictMode>
 );
